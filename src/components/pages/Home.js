cc82452cc03d903dacb474315bedfe32e045dd34
Linus Wallin <linuswallin@live.se>
2024-05-16 16:07:21 +0200

    feat: adds home page wiht video

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import "../Home.css"
 
 function Home() {
   return (
-    <div>
-    </div>
+    <>
+      <div className='video-container'>
+        <video src='/videos/environmentvideo.webm' autoPlay muted loop/>
+        <div className='video-text-container'>
+          <p className='video-title'>
+            ALIEN ODYSSEY
+          </p>
+          <p className='video-sub-title'>
+            EXPLORE, CRAFT, SURVIVE
+          </p>
+          <p className='video-sub-title'>
+            BUT MOST IMPORTANTLY: MAKE IT BACK TO YOUR PLANET
+          </p>
+        </div>  
+      </div>
+      <div className='info-container'>
+        Info about the game
+      </div>
+    </>
+    
   )
 }
 
